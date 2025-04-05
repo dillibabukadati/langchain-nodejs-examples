c97da3474b76cac40c1d595a4bfce01af68d17ad
Dilli Babu Kadati <dillibabukadati@gmail.com>
2025-04-05 09:57:30 +0530

    Added knowledge base and code for CAG

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { sendMessage } from "./model.js";
 import cors from "cors";
+import { fileURLToPath } from "url";
+import path from "path";
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
@@ -9,6 +11,14 @@ app.post("/chat", async (req, res) => {
   const { message } = req.body;
   const response = await sendMessage(message);
   res.send({ message: response });
+});
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(__dirname));
+
+app.get("/", (_, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
--- model.js
+++ model.js
@@ -1,22 +1,30 @@
 import { ChatGroq } from "@langchain/groq";
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import fs from "fs";
+
 import dotenv from "dotenv";
 dotenv.config();
 
 const model = new ChatGroq({
-  model: "llama3-8b-8192",
+  model: "llama-3.3-70b-versatile",
   temperature: 0,
 });
 
+// Reading the knowledge base from the file
+const knowledgeBase = fs.readFileSync("./paracetamol.txt", "utf8");
 const messages = [
   new SystemMessage(
-    `Your name is Helix. You are simple helpful chatbot. You answers to user queries in a friendly manner.
-    Make your response simpler`
+    `Your name is Helix and are an AI assistant with preloaded knowledge. Use the provided knowledge to answer user queries in a simple and friendly manner.`
   ),
+  // Adding the knowledge base to the messages
+  new SystemMessage(`Knowledge base: ${knowledgeBase}`),
+  new SystemMessage(`If you don't know the answer, say "I don't know". And say you can answer only about topic from the knowledge base.`),
 ];
 
 export const sendMessage = async (message) => {
   messages.push(new HumanMessage(message));
+  console.log("User message:", message);
   const response = await model.invoke(messages);
+  console.log("Model response:", response);
   return response.content;
 };
