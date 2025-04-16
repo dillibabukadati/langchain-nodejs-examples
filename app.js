65f2d1c7cb49e00db39e581e9daa8d2cbb7c020b
Dilli Babu Kadati <dillibabukadati@gmail.com>
2025-04-16 08:34:09 +0530

    Added index.html and its serving for demo of chatbot

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { sendMessage } from "./model.js";
 import cors from "cors";
+import { fileURLToPath } from "url";
+import path from "path";
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
@@ -11,6 +13,14 @@ app.post("/chat", async (req, res) => {
   res.send({ message: response });
 });
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(__dirname));
+
+app.get("/", (_, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
